ba30631a0c2c836dd3f905fdf965f1e1216b61d3
Jaimin <jaimin.ece@gmail.com>
2025-11-10 00:58:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Leetcode15Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
