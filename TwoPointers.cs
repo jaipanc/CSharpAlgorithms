23738d74453ed6b7bba96beb02f9321bf84fd554
Jaimin <jaimin.ece@gmail.com>
2025-11-11 23:34:09 -0500

    ThreeSum

--- TwoPointers.cs
+++ TwoPointers.cs
@@ -68,5 +68,50 @@
             }
             return result;
         }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums); // Step 1: Sort the array
+            var result = new List<IList<int>>();
+            int n = nums.Length;
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                // Skip duplicate values for i
+                if (i > 0 && nums[i] == nums[i - 1])
+                    continue;
+
+                int left = i + 1;
+                int right = n - 1;
+
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+
+                    if (sum == 0)
+                    {
+                        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                        // Move left pointer to next unique number
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        // Move right pointer to previous unique number
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    }
+                    else if (sum < 0)
+                    {
+                        left++; // Need a bigger sum
+                    }
+                    else
+                    {
+                        right--; // Need a smaller sum
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
