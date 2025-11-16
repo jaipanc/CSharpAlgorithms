c80feb50a31f80906a2f89cb3c05888c41f55575
Jaimin <jaimin.ece@gmail.com>
2025-11-16 18:09:38 -0500

    Can Attent Meeting

--- Intervals.cs
+++ Intervals.cs
@@ -8,5 +8,25 @@ namespace Leetcode15Patterns
 {
     static internal class Intervals
     {
+        public static bool CanAttendMeetings(int[][] intervals)
+        {
+            if (intervals is null || intervals.Length == 0)
+            {
+                return true;
+            }
+
+            // Sort intervals based on start time
+            Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                // If the start time of the current interval is less than the end time of the previous interval, return false
+                if (intervals[i][0] < intervals[i - 1][1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 }
