cef076a3027edcf6f8660d2001ab7e5a9cd03950
Jaimin <jaimin.ece@gmail.com>
2025-11-29 20:00:06 -0500

    new class of Hashing and Dictionary Problems from Leetcode

---
+++ Leetcode15Patterns/DictionaryAndHashing.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode15Patterns
+{
+    public static class DictionaryAndHashing
+    {
+        public static int[] LC1_TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+                if (map.TryGetValue(complement, out int index))
+                    return [index, i];
+                map[nums[i]] = i;
+            }
+            return [];
+        }
+    }
+}
