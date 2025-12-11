a0d4d8bef8feae78a5f5777f55955baf1eb1b5ba
Jaimin <jaimin.ece@gmail.com>
2025-12-10 23:53:50 -0500

    New Algo : Sliding Window

---
+++ Leetcode15Patterns/SlidingWindow.cs
@@ -0,0 +1,29 @@
+﻿namespace Leetcode15Patterns
+{
+    public class SlidingWindow
+    {
+        public static int FruitsIntoBasket(int[] fruits)
+        {
+            int start = 0;
+            Dictionary<int, int> Basket = [];
+            int mxFruit = 0;
+
+            for (int end = 0; end < fruits.Length; end++)
+            {
+                if (Basket.TryGetValue(end, out int value))
+                {
+                    Basket[end] = value + 1;
+                }
+                else
+                {
+                    Basket[end] = 1;
+                }
+            }
+
+            while(Basket.Count > 2)
+            {
+                var startFruit = Basket[start];
+            }
+        }
+    }
+}
