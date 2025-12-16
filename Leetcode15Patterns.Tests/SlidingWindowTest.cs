bff1cdde12f5a5aeed0f5b29d2ea5b34037b7397
Jaimin <jaimin.ece@gmail.com>
2025-12-16 00:36:43 -0500

    Unit Test  : 2461. Maximum Sum of Distinct Subarrays With Length K

--- Leetcode15Patterns.Tests/SlidingWindowTest.cs
+++ Leetcode15Patterns.Tests/SlidingWindowTest.cs
@@ -36,5 +36,16 @@ namespace Leetcode15Patterns.Tests
             var output = SlidingWindow.MaxSumSubArray(nums, k);
             Assert.Equal(expected, output);
         }
+
+        [Theory]
+        [InlineData(new int[] { 1, 5, 4, 2, 9, 9, 9 }, 3, 15)]
+        [InlineData(new int[] { 4, 4, 4 }, 3, 0)]
+        [InlineData(new int[] { 5, 5, 5, 5, 5 }, 3, 0)]
+        [InlineData(new int[] { 3, 2, 2, 3, 4, 6, 7, 7, -1 }, 4, 20)]
+        public void TestMaximumSumOfDistinctSubArray(int[] nums, int k, int expected)
+        {
+            var output = SlidingWindow.MaximumSumOfDistinctSubArray(nums, k);
+            Assert.Equal(expected, output);
+        }
     }
 }
