93c3e6adc929c458883fb598d9b365ff7d51b701
Jaimin <jaimin.ece@gmail.com>
2025-12-24 15:53:38 -0500

    Unit Test  - 759. Employee Free Time

--- Leetcode15Patterns.Tests/IntervalsTests.cs
+++ Leetcode15Patterns.Tests/IntervalsTests.cs
@@ -1,4 +1,6 @@
-﻿namespace Leetcode15Patterns.Tests
+﻿using System.Numerics;
+
+namespace Leetcode15Patterns.Tests
 {
     public class IntervalsTests
     {
@@ -39,6 +41,23 @@
                 { new int[][]{[1,2],[2,3]}, 0 }
             };
             return data;
+        }
+
+        [Theory]
+        [MemberData(nameof(EmployeeFreeTimeTestData))]
+        public void TestEmployeeFreeTime(int[][][] schedules, int[][] common)
+        {
+            var output = Intervals.EmployeeFreeTime(schedules);
+            Assert.Equal(common, output);
+        }
+
+        public static TheoryData<int[][][], int[][]> EmployeeFreeTimeTestData()
+        {
+            return new TheoryData<int[][][], int[][]>
+            {
+                {new int[][][]{[[1,2],[5,6]],[[1,3]],[[4,10]]}, new int[][]{[3,4]} },
+                {new int[][][]{[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]}, new int[][]{ [5, 6], [7, 9] } }
+            };
         }
     }
 }
