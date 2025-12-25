c3a6fdb1b768ac5bfd459b36b60cfd402080e845
Jaimin <jaimin.ece@gmail.com>
2025-12-24 21:48:44 -0500

    Unit Test  - 20. Valid Parentheses

---
+++ Leetcode15Patterns.Tests/TestStackPattern.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Leetcode15Patterns.Tests
+{
+    public class TestStackPattern
+    {
+        [Theory]
+        [InlineData("(){({})}", true)]
+        [InlineData("(){({}})", false)]
+        public void TestIsValidString(string s, bool isValid)
+        {
+            var output = StackPattern.IsValid(s);
+            Assert.Equal(isValid, output);
+        }
+    }
+}
