7a32f52f159ed5b9e5db0628692b5facedbb5b91
Jaimin <jaimin.ece@gmail.com>
2025-12-25 23:46:18 -0500

    Unit Test 394. Decode String

--- Leetcode15Patterns.Tests/TestStackPattern.cs
+++ Leetcode15Patterns.Tests/TestStackPattern.cs
@@ -11,5 +11,15 @@ namespace Leetcode15Patterns.Tests
             var output = StackPattern.IsValid(s);
             Assert.Equal(isValid, output);
         }
+
+        [Theory]
+        [InlineData("3[a2[c]]", "accaccacc")]
+        [InlineData("3[a]2[bc]", "aaabcbc")]
+        [InlineData("2[abc]3[cd]ef", "abcabccdcdcdef")]
+        public void TestDecodeString(string encoded,string decoded)
+        {
+            var output = StackPattern.DecodeString(encoded);
+            Assert.Equal(decoded, output);
+        }
     }
 }
