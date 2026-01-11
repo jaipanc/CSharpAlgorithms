00048952b2e27aa1a925e81391b549f375e72085
Jaimin <jaimin.ece@gmail.com>
2026-01-10 23:51:27 -0500

    Unit Test Matrices

---
+++ CSharpAlgorithms.Tests/MatricesTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CSharpAlgorithms.Tests
+{
+    public class MatricesTest
+    {
+        [Theory]
+        [MemberData(nameof(SpiralOrderTestData))]
+        public void TestSpiralOrder(int[][] matrix, List<int> expected)
+        {
+            var output = Matrices.SpiralOrder(matrix);
+            Assert.Equal(expected, output);
+        }
+
+        public static TheoryData<int[][], List<int>> SpiralOrderTestData()
+        {
+            var data = new TheoryData<int[][], List<int>>
+            {
+                { new int[][] { [0,1,2], [3,4,5], [6,7,8] }, new List<int>{0,1,2,5,8,7,6,3,4 } }
+            };
+            return data;
+        }
+    }
+}
