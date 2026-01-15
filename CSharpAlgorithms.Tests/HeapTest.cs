ee84885e5fb43f7a580adf60567143e193ddd40e
Jaimin <jaimin.ece@gmail.com>
2026-01-14 23:44:36 -0500

    Unit Test | Heap : 3 largest elements from int array

---
+++ CSharpAlgorithms.Tests/HeapTest.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpAlgorithms.Tests
+{
+    public class HeapTest
+    {
+        [Theory]
+        [InlineData(new int[] { 9, 3, 7, 1, -2, 6, 8 }, new int[] { 7, 9, 8 })]
+        public void TestHeap(int[] nums, int[] expected)
+        {
+            var output = Heap.ThreeLargest(nums);
+            Assert.Equal(expected, output);
+        }
+    }
+}
