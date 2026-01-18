abdee62283d4c1dfc4f8eb7c9c9502ff75905ea5
Jaimin <jaimin.ece@gmail.com>
2026-01-17 23:16:10 -0500

    Unit Test :  K Closest Points to Origin

--- CSharpAlgorithms.Tests/HeapTest.cs
+++ CSharpAlgorithms.Tests/HeapTest.cs
@@ -9,5 +9,23 @@
             var output = Heap.ThreeLargest(nums);
             Assert.Equal(expected, output);
         }
+
+        [Theory]
+        [MemberData(nameof(KClosetToOriginTestData))]
+        public void TestKClosetToOrigin(int[][] nums, int k, int[][] exptected)
+        {
+            var output = Heap.KClosetToOrigin(nums, k);
+            Assert.Equivalent(exptected, output);
+        }
+
+        public static TheoryData<int[][],int, int[][]> KClosetToOriginTestData()
+        {
+            return new TheoryData<int[][], int, int[][]>()
+            {
+                {new int[][]{ [1, 3], [-2, 2] } , 1 , new int[][]{[-2,2]} },
+                {new int[][]{ [3, 3], [5, -1], [-2, 4] } , 2 , new int[][]{ [3, 3], [-2, 4] } },
+                {new int[][]{ [3, 4], [2, 2], [1, 1], [0, 0], [5, 5] } , 3 , new int[][]{ [2, 2], [1, 1], [0, 0] } }
+            };
+        }
     }
 }
