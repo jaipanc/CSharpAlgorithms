f2186576746c0dda2b79927500b0fe1d482b6021
Jaimin <jaimin.ece@gmail.com>
2026-04-04 23:10:00 -0400

    Unit test 338. Counting Bits

--- CSharpAlgorithms.Tests/DynamicProgrammingTest.cs
+++ CSharpAlgorithms.Tests/DynamicProgrammingTest.cs
@@ -20,4 +20,17 @@ public class DynamicProgrammingTest
         var result = DynamicProgramming.Rob(nums);
         Assert.Equal(expected, result);
     }
+
+    [Theory]
+    [InlineData(0, new int[] { 0 })]
+    [InlineData(1, new int[] { 0, 1 })]
+    [InlineData(2, new int[] { 0, 1, 1 })]
+    [InlineData(3, new int[] { 0, 1, 1, 2 })]
+    [InlineData(4, new int[] { 0, 1, 1, 2, 1 })]
+    [InlineData(7, new int[] { 0, 1, 1, 2, 1, 2, 2, 3 })]
+    public void CountingBitsTest(int n, int[] expected)
+    {
+        var result = DynamicProgramming.CountBits(n);
+        Assert.Equal(expected, result);
+    }
 }
