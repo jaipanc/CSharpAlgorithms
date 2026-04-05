cfabfe708efa0d93a64108d6e5666fd47786b3c6
Jaimin <jaimin.ece@gmail.com>
2026-04-04 23:09:54 -0400

    338. Counting Bits using Dynamic Programming

--- CSharpAlgorithms/DynamicProgramming.cs
+++ CSharpAlgorithms/DynamicProgramming.cs
@@ -25,4 +25,21 @@ public class DynamicProgramming
         }
         return curr;
     }
+
+    /// <summary>
+    /// 338. Counting Bits
+    /// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static int[] CountBits(int n)
+    {
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            dp[i] = dp[i / 2] + (i % 2);
+        }
+        return dp;
+    }
 }
