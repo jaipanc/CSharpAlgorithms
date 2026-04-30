dbb00c064a648ba58a41614d728f018430e14f35
Jaimin <jaimin.ece@gmail.com>
2026-04-30 18:22:07 -0400

    Unit Test : 33. Search in Rotated Sorted Array

--- CSharpAlgorithms.Tests/BinarySearchTest.cs
+++ CSharpAlgorithms.Tests/BinarySearchTest.cs
@@ -29,5 +29,35 @@
             var output = binarySearch.SearchInsert(nums, target);
             Assert.Equal(expected, output);
         }
+
+        [Theory]
+        // Target found in left sorted half
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 5, 1)]
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 4, 0)]
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 6, 2)]
+        // Target found in right sorted half
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 1, 5)]
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 2, 6)]
+        // Target not found
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 8, -1)]
+        // Edge cases
+        [InlineData(new int[] { 1 }, 1, 0)]
+        [InlineData(new int[] { 1 }, 0, -1)]
+        [InlineData(new int[] { 1, 3 }, 3, 1)]
+        [InlineData(new int[] { 1, 3 }, 1, 0)]
+        [InlineData(new int[] { 3, 1 }, 1, 1)]
+        [InlineData(new int[] { 3, 1 }, 3, 0)]
+        // Larger rotated arrays
+        [InlineData(new int[] { 5, 1, 3 }, 3, 2)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 2, 1)]
+        [InlineData(new int[] { 7, 1, 2, 3, 4, 5, 6 }, 2, 2)]
+        public void TestSearchRotatedArray(int[] nums, int target, int expected)
+        {
+            var binarySearch = new BinarySearch();
+            var output = binarySearch.SearchRotatedArray(nums, target);
+            Assert.Equal(expected, output);
+        }
     }
 }
