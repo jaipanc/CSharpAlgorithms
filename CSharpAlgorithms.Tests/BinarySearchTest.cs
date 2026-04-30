2faaa1a2bf2002119bc46b566d4dffcc7f6d2485
Jaimin <jaimin.ece@gmail.com>
2026-04-29 21:09:09 -0400

    Unit Test : 35. Search Insert Position

--- CSharpAlgorithms.Tests/BinarySearchTest.cs
+++ CSharpAlgorithms.Tests/BinarySearchTest.cs
@@ -13,5 +13,21 @@
             var output = BinarySearch.FindTarget(arr, target);
             Assert.Equal(expected, output);
         }
+
+        [Theory]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 5, 2)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 2, 1)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 7, 4)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 0, 0)]
+        [InlineData(new int[] { 1 }, 1, 0)]
+        [InlineData(new int[] { 1 }, 0, 0)]
+        [InlineData(new int[] { 1 }, 2, 1)]
+        [InlineData(new int[] { 1, 3 }, 2, 1)]
+        public void TestSearchInsert(int[] nums, int target, int expected)
+        {
+            var binarySearch = new BinarySearch();
+            var output = binarySearch.SearchInsert(nums, target);
+            Assert.Equal(expected, output);
+        }
     }
 }
